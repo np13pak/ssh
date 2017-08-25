6933a7bf6b144e1b9b36d261df99126e134e2e93
np13pak <30809073+np13pak@users.noreply.github.com>
2017-08-25 13:16:51 +0300

    Update README.bash

--- README.bash
+++ README.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo 'ubuntu:ubuntu' |chpasswd
 
-echo "PermitRootLogin without-password" >> /etc/ssh/sshd_config
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+sed -ri 's/^PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -ri 's/UsePAM yes/#UsePAM yes/g' /etc/ssh/sshd_config
