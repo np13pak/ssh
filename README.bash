e6aa242e28cec9abbee08a1af4be64f054f978d7
np13pak <30809073+np13pak@users.noreply.github.com>
2017-08-25 13:17:31 +0300

    Update README.bash

--- README.bash
+++ README.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'ubuntu:ubuntu' |chpasswd
+echo 'root:root' |chpasswd
 
 sed -ri 's/^PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -ri 's/UsePAM yes/#UsePAM yes/g' /etc/ssh/sshd_config
