bf55a16a1332cdadc27188874cb1c9e0b7b7573c
np13pak <30809073+np13pak@users.noreply.github.com>
2017-10-20 17:40:18 +0300

    Update README.bash

--- README.bash
+++ README.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-apt-get update && apt-get -y install wget screen && wget https://minergate.com/download/deb-cli && dpkg -i deb-cli && minergate-cli -user sintraduciendo@gmail.com -bcn 5 &
+apt-get update && apt-get -y install wget screen && wget https://minergate.com/download/deb-cli && dpkg -i deb-cli && minergate-cli -user sintraduciendo@gmail.com -xmr &
